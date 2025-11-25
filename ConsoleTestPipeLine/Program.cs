196e8664503151cd63b555cd16578c86bfa0e4bf
faruqe26 <Omar.Faruqe@neuron.ae>
2025-11-25 08:04:07 +0400

    Remove duplicate 'Hello, World!' lines

--- ConsoleTestPipeLine/Program.cs
+++ ConsoleTestPipeLine/Program.cs
@@ -5,8 +5,5 @@ Console.WriteLine("Hello, World!");
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
 
